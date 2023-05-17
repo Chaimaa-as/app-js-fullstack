4a6231a6b7b5e9a6e76a56a89e5cdcf91181d836
ChaimaaAsri <108627246+Chaimaa-as@users.noreply.github.com>
2023-05-17 18:44:49 +0200

    controllers

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,39 +1,24 @@
 const express = require("express");
+const {
+  getPosts,
+  setPosts,
+  editPost,
+  deletePost,
+  likePost,
+  dislikePost,
+} = require("../controllers/post.controller");
 
 // on va utiliser l'objet de route de express
 const router = express.Router();
 
-// on recupere ce qu'on avait mis dans le serveur et on remplace app par router et on supprime "post"
+router.get("/", getPosts);
+router.post("/", setPosts);
+router.put("/:id", editPost);
+router.delete("/:id", deletePost);
 
-// app.get("/post", (req, res) => {
-//   res.json({ message: "voici les données choupette" });
-// });
-router.get("/", (req, res) => {
-  res.json({ message: "voici les données choupette" });
-});
+router.patch("/like-post/:id", likePost);
 
-router.post("/", (req, res) => {
-  //   console.log(req.body);c'était pour vérifier
-  res.json({ message: req.body.message });
-});
-
-router.put("/:id", (req, res) => {
-  res.json({ messageId: req.params.id });
-});
-
-// donc maintenant pour faire le CRUD je vais rajouter la route delete
-router.delete("/:id", (req, res) => {
-  res.json({ message: "Post supprimé id : " + req.params.id });
-});
-// on veut que chaque route génère un tableau avec les personnes avec leur id associé qui ont liké ce post
-router.patch("/like-post/:id", (req, res) => {
-  res.json({ message: "Post liké : " + req.params.id });
-});
-
-//idem pour les dislikes
-router.patch("/dislike-post/:id", (req, res) => {
-  res.json({ message: "Post disliké : " + req.params.id });
-});
+router.patch("/dislike-post/:id", dislikePost);
 
 // Export du module : comme ça tt notre projet aura accès à ce qui va se passer ici
 module.exports = router;
